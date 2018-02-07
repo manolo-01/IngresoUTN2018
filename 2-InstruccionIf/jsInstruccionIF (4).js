4ea260d75ab2e630269b8e86f45cb26430b0e716
manolo-01 <36174658+manolo-01@users.noreply.github.com>
2018-02-07 12:52:02 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var edad;
+edad=parseInt(document.getElementById("edad").value);
+if((12<edad) && (edad<18))
+alert("usted es adolescente");
+else
+alert("usted no es un adolescente");
 
 
 }//FIN DE LA FUNCIÓN
