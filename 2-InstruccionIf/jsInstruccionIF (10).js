ddb99ff4e072703555070352fd49322d38dd823f
manolo-01 <36174658+manolo-01@users.noreply.github.com>
2018-02-08 21:52:59 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,17 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var dato;
+	dato=parseInt((Math.random()*10));
+
+	if(dato<4)
+	alert("nota: "+dato+", vamos la proxima se puede.");
+	  else{
+		  if(dato<9)
+			alert("nota: "+dato+", aprobo!.");
+			else
+			alert("nota: "+dato+", excelente!.");
+	  }
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -5,6 +5,6 @@ function Mostrar()
 var edad;
 edad=parseInt(document.getElementById("edad").value);
 if(edad>=18)
-{alert("usted es mayor de edad")};
-
+{alert("usted es mayor de edad");
+}
 }//FIN DE LA FUNCIÓN
