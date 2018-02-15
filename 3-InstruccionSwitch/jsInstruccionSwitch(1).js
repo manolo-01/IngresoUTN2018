1b44b5de78d26683303a83dda1ef32b2558c545f
manolo-01 <36174658+manolo-01@users.noreply.github.com>
2018-02-15 16:17:21 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,25 @@
 function Mostrar()
 {
 //tomo el mes
-var mesDelAño = document.getElementById('mes').value;
 
 
+var mes;
+mes=document.getElementById("FormIngreso").value;
+switch(mes)
+{
+    case "Enero":
+    alert("que comiences bien el año!!!.");
+    break;
+    case "Marzo":
+    alert("a clases!!!.");
+    break;
+    case "Julio":
+    alert("se vienen las vacaciones!!!.");
+    break;
+    case "Diciembre":
+    alert("Felices Fiestas!!!.");
+    break;
+}
 
 
 }//FIN DE LA FUNCIÓN
